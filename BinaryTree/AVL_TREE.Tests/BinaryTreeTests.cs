df3f80e2b35eb143776772aa0d69e8bf692194cd
Vladislav Kotov <vlad.kotov1999@ya.ru>
2020-06-16 03:05:23 +0300

    .
    Balanced Binary Tree

---
+++ BinaryTree/AVL_TREE.Tests/BinaryTreeTests.cs
@@ -0,0 +1,113 @@
+﻿using BalancedBinaryTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AVL_TREE.Tests
+{
+    [TestClass]
+    public class BinaryTreeTests
+    {
+        private BinaryTree<int> tree;
+
+        #region Search Tests
+
+        [TestMethod]
+        public void Search_ExistNode_trueReturned()
+        {
+            tree = new BinaryTree<int>(3);
+          
+            tree.Insert(1);
+            tree.Insert(5);
+           
+            Assert.IsTrue(tree.Search(5));
+        }
+
+        [TestMethod]
+        public void Search_EmptyTree_falseReturned()
+        {
+            tree = new BinaryTree<int>(3);
+            tree.Erase(3);
+            Assert.IsFalse(tree.Search(3));
+            Assert.IsFalse(tree.Search(5));
+        }
+
+        [TestMethod]
+        public void Search_NonxistentNode_falseReturned()
+        {
+ 
+            tree = new BinaryTree<int>(3);
+           
+            tree.Insert(1);
+            tree.Insert(5);
+  
+            Assert.IsFalse(tree.Search(42));
+        }
+
+
+
+        #endregion
+
+        #region Erase Tests
+        [TestMethod]
+        public void Erase_Delete5_5Deleted()
+        {
+
+            tree = new BinaryTree<int>(2);
+           
+            tree.Insert(5);
+            tree.Insert(43);
+            tree.Erase(5);
+           
+            Assert.IsFalse(tree.Search(5));
+        }
+
+        public void Erase_EmptyTree_NothingReturned()
+        {
+            
+            tree = new BinaryTree<int>(2);
+
+            tree.Erase(2);
+
+            Assert.IsFalse(tree.Search(53));
+        }
+
+        [TestMethod]
+        public void Erase_NonexistentNode_NothingReturned()
+        {
+            
+            tree = new BinaryTree<int>(2);
+            
+            tree.Insert(5);
+            tree.Insert(43);
+            tree.Erase(53);
+            
+            Assert.IsFalse(tree.Search(53));
+        }
+        #endregion
+        [TestMethod]
+
+        public void Insert_EmptyTree_AddedNode()
+        {
+            tree = new BinaryTree<int>(2);
+
+            tree.Erase(2);
+            Assert.IsFalse(tree.Search(2));
+            tree.Insert(1);
+            
+            Assert.IsTrue(tree.Search( 1));
+        }
+
+        public void Insert_ADD5_NothingReturned()
+        {
+            
+            tree = new BinaryTree<int>(41);
+
+            bool expected = true;
+
+            
+            tree.Insert(5);
+            bool actual = tree.Search(5);
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ BinaryTree/AVL_TREE.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AVL_TREE.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("AVL_TREE.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1e9e2d9a-bad1-41b4-a3e6-54aa9c962660")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
